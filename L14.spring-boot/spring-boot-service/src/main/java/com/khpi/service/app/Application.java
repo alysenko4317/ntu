90badbcca4ec34e8676be178d9d6c313f5ffd50b
Anton Lysenko <lysenko@qarea.com>
2023-02-19 18:29:38 +0200

    L14 - spring-boot samples - initial commit

---
+++ L14.spring-boot/spring-boot-service/src/main/java/com/khpi/service/app/Application.java
@@ -0,0 +1,46 @@
+package com.khpi.service.app;
+
+import freemarker.template.TemplateException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+import java.io.IOException;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.khpi.service")
+@EnableJpaRepositories(basePackages = "com.khpi.service.repositories")
+@EntityScan(basePackages = "com.khpi.service.models")
+public class Application
+{
+    @Bean
+    public ViewResolver viewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(true);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        resolver.setContentType("text/html; charset=UTF-8");
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freemarkerConfig() throws IOException, TemplateException {
+        FreeMarkerConfigurationFactory factory = new FreeMarkerConfigurationFactory();
+        factory.setTemplateLoaderPaths("classpath:templates", "src/main/resource/templates");
+        factory.setDefaultEncoding("UTF-8");
+        FreeMarkerConfigurer result = new FreeMarkerConfigurer();
+        result.setConfiguration(factory.createConfiguration());
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
